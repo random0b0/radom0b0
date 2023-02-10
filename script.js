c77d2bf4ca135ccd839b5f5eab6f3ebc78f89018
random0b0 <125090776+random0b0@users.noreply.github.com>
2023-02-10 21:57:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+<><button id="myButton">Click Me!</button><script>const button = document.querySelector("#myButton");
+    button.addEventListener("click", function() {alert("Button was clicked!")});
+</script></>
